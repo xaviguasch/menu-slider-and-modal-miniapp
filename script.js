cf5a7d9e84a3821db0035e30f916db514a93710d
xaviguasch <xaviguasch@gmail.com>
2020-02-14 17:33:03 +0100

    added menu toggling and showing and closing of modal with js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('it works!!!')
+const toggle = document.querySelector('#toggle')
+const close = document.querySelector('#close')
+const open = document.querySelector('#open')
+const modal = document.querySelector('#modal')
+
+// Toggle nav
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'))
+
+// show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'))
+
+// hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'))
+
+// hide modal on outside click
+window.addEventListener('click', e =>
+  e.target === modal ? modal.classList.remove('show-modal') : false
+)
